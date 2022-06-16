6221dfbfb473d48f521c46815117eabcf29d0dc8
unknown <flucasjs@gmail.com>
2022-06-15 22:34:24 -0700

    Update BurgerButton and ExitButton to extend MenuButton
    BurgerButton and ExitButton repeat many of the same styles. Creating a
    parent component that encapsulates these repeated styles will make the
    project more maintainable.

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,7 +29,7 @@ const backgroundStyles = css`
   }
 `
 
-const BurgerButton = styled.button`
+const MenuButton =  styled.button`
   display: none;
 
   @media (max-width: 35rem) {
@@ -40,30 +40,28 @@ const BurgerButton = styled.button`
     top: 2rem;
     right: 1rem;
     background-color: transparent;
-    background-image: url(${hamburgerIcon});
+    background-position: center;
     background-repeat: no-repeat;
     width: 1.5rem;
     aspect-ratio: 1;
     border: 0;
   }
+
+  &:focus-visible {
+    outline: 5px solid white;
+    outline-offset: 5px;
+  }
 `
 
-const ExitButton = styled.button`
-  display: none;
-
+const BurgerButton = styled(MenuButton)`
   @media (max-width: 35rem) {
-    cursor: pointer;
-    display: block;
-    position: absolute;
-    z-index: 2000;
-    top: 2rem;
-    right: 1rem;
-    background-color: transparent;
+    background-image: url(${hamburgerIcon});
+  }
+`
+
+const ExitButton = styled(MenuButton)`
+  @media (max-width: 35rem) {
     background-image: url(${closeIcon});
-    background-repeat: no-repeat;
-    width: 1.5rem;
-    aspect-ratio: 1;
-    border: 0;
   }
 `
 
