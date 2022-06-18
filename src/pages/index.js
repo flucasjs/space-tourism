b7e342dd038ba0cf7dbc5c10d90fba9db12bad3e
unknown <flucasjs@gmail.com>
2022-06-18 10:49:55 -0700

    Update import statements

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,18 @@
-import React, { useState, useEffect } from 'react'
-import styled, {css} from 'styled-components'
+import React, { useState } from 'react'
+import {css} from 'styled-components'
 import Layout from '../components/Layout'
 import { StaticImage} from 'gatsby-plugin-image'
-import PrimaryNavigation, { PrimaryNavigationWrapper } from '../components/PrimaryNavigation'
+import PrimaryHeader from '../components/PrimaryHeader'
+import PrimaryNavigation from '../components/PrimaryNavigation'
+import PrimaryNavigationWrapper from '../components/PrimaryNavigationWrapper'
+import BurgerButton from '../components/BurgerButton'
+import ExitButton from '../components/ExitButton'
+import SkipToContentButton from '../components/SkipToContentButton'
+import LogoContainer from '../components/LogoContainer'
 
 import backgroundMobile from '../assets/home/background-home-mobile.jpg'
 import backgroundTablet from '../assets/home/background-home-tablet.jpg'
 import backgroundDesktop from '../assets/home/background-home-desktop.jpg'
-
-import hamburgerIcon from '../assets/shared/icon-hamburger.svg'
-import closeIcon from '../assets/shared/icon-close.svg'
-
-const PrimaryHeader = styled.header`
-  justify-content: space-between;
-  align-items: center;
-
-  @media (min-width: 45rem) {
-    &::after {
-      content: '';
-      display: block;
-      position: relative;
-      height: 1px;
-      width: 100%;
-      order: 1;
-      margin-right: -2.5rem;
-      background-color: hsl( var(--clr-white) / 0.25 );
-    }
-  }
-`
 
 const backgroundStyles = css`
   background-image: url(${backgroundMobile});
@@ -41,61 +26,6 @@ const backgroundStyles = css`
   @media (min-width: 45rem) {
     background-image: url(${backgroundDesktop});
     background-position: bottom center;
-  }
-`
-
-const MenuButton =  styled.button`
-  display: none;
-
-  @media (max-width: 35rem) {
-    cursor: pointer;
-    display: block;
-    position: absolute;
-    z-index: 2000;
-    top: 2rem;
-    right: 1rem;
-    background-color: transparent;
-    background-position: center;
-    background-repeat: no-repeat;
-    width: 1.5rem;
-    aspect-ratio: 1;
-    border: 0;
-  }
-
-  &:focus-visible {
-    outline: 5px solid white;
-    outline-offset: 5px;
-  }
-`
-
-const BurgerButton = styled(MenuButton)`
-  @media (max-width: 35rem) {
-    background-image: url(${hamburgerIcon});
-  }
-`
-
-const ExitButton = styled(MenuButton)`
-  @media (max-width: 35rem) {
-    background-image: url(${closeIcon});
-  }
-`
-
-const LogoContainer = styled.div`
-  margin: 1.5rem clamp(1.5rem, 5vw, 3.5rem);
-`
-
-const SkipToContentButton = styled.a`
-  position: absolute;
-  z-index: 9999;
-  background-color: hsl( var(--clr-white) );
-  color: hsl( var(--clr-dark) );
-  padding: .5em 1em;
-  margin-inline: auto;
-  transform: translateY(-100%);
-  transition: transform 250ms ease-out;
-
-  &:focus {
-    transform: translateY(0);
   }
 `
 
